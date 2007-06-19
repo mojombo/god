494186666e782dc999c0b9ee8e9287b76e4d66bd
Tom Preston-Werner <tom@mojombo.com>
2007-06-18 23:17:04 -0700

    updated manifest, added behavior tests

--- test/test_behavior.rb
+++ test/test_behavior.rb
@@ -1,9 +1,13 @@
 require 'helper'
 
 class TestBehavior < Test::Unit::TestCase
-  def setup
-    @behavior = Behavior.new
+  def test_generate_should_return_an_object_corresponding_to_the_given_type
+    assert_equal Behaviors::FakeBehavior, Behavior.generate(:fake_behavior).class
   end
   
-  
+  def test_generate_should_raise_on_invalid_type
+    assert_raise NoSuchBehaviorError do
+      Behavior.generate(:foo)
+    end
+  end
 end
