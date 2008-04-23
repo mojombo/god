9ca1b565436f7ab863275b0fbc9db157374cb787
Mike Mondragon <mikemondragon@gmail.com>
2008-04-23 00:34:35 -0700

    tighten up God::Contacts::Email with some tests

---
+++ test/test_email.rb
@@ -0,0 +1,43 @@
+require File.dirname(__FILE__) + '/helper'
+
+class TestEmail < Test::Unit::TestCase
+  def test_exists
+    God::Contacts::Email
+  end
+
+  def test_unknown_delivery_method_for_notify
+    assert_nothing_raised do
+      God::Contacts::Email.any_instance.expects(:notify_smtp).never
+      God::Contacts::Email.any_instance.expects(:notify_sendmail).never
+      God::Contacts::Email.delivery_method = :foo_protocol
+      g = God::Contacts::Email.new
+      g.notify(:a, :b, :c, :d, :e)
+      assert_equal "failed to send email to : unknown delivery method: foo_protocol", g.info
+    end
+  end
+
+  def test_smtp_delivery_method_for_notify
+    assert_nothing_raised do
+      God::Contacts::Email.any_instance.expects(:notify_sendmail).never
+      God::Contacts::Email.any_instance.expects(:notify_smtp).once.returns(nil)
+      God::Contacts::Email.delivery_method = :smtp
+      g = God::Contacts::Email.new
+      g.email = 'joe@example.com'
+      g.notify(:a, :b, :c, :d, :e)
+      assert_equal "sent email to joe@example.com", g.info
+    end
+  end
+  
+  def test_sendmail_delivery_method_for_notify
+    assert_nothing_raised do
+      God::Contacts::Email.any_instance.expects(:notify_smtp).never
+      God::Contacts::Email.any_instance.expects(:notify_sendmail).once.returns(nil)
+      God::Contacts::Email.delivery_method = :sendmail
+      g = God::Contacts::Email.new
+      g.email = 'joe@example.com'
+      g.notify(:a, :b, :c, :d, :e)
+      assert_equal "sent email to joe@example.com", g.info
+    end
+  end
+  
+end
