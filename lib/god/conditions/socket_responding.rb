38d230a5011b6143af4e1a75273935ec92c66c4d
Steven Jenkins <steven@railsdog.com>
2010-09-23 23:39:27 -0400

    add support for socket method -- no tests yet

--- lib/god/conditions/socket_responding.rb
+++ lib/god/conditions/socket_responding.rb
@@ -4,13 +4,27 @@ include Socket::Constants
 module God
   module Conditions
     class SocketResponding < PollCondition
-      attr_accessor :family, :addr, :port, :socket, :path
+      attr_accessor :family, :addr, :port, :path
      
       def initialize
         super
+        # default to tcp on the localhost
         self.family = 'tcp'
         self.addr = '127.0.0.1'
       end 
+
+      def socket=(s)
+        components = s.split(':')
+        if components.size == 3
+          @family,@addr,@port = components
+        elsif components[0] =~ /^tcp$/
+          @family = components[0]
+          @port = components[1]
+        elsif components[0] =~ /^unix$/
+          @family = components[0]
+          @path = components[1]
+        end
+      end
 
       def valid?
         valid = true
