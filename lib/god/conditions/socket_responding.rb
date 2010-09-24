e0baf491c78f1cfb952a1cfebfe6073c50f6c005
Steven Jenkins <steven@railsdog.com>
2010-09-23 23:22:00 -0400

    creation tests pass; now starting socket() tests

--- lib/god/conditions/socket_responding.rb
+++ lib/god/conditions/socket_responding.rb
@@ -1,15 +1,34 @@
+require 'socket'
+include Socket::Constants
+
 module God
   module Conditions
     class SocketResponding < PollCondition
-      attr_accessor :family, :addr, :port
-      
+      attr_accessor :family, :addr, :port, :socket, :path
+     
+      def initialize
+        super
+        self.family = 'tcp'
+        self.addr = '127.0.0.1'
+      end 
+
       def valid?
         valid = true
+        if self.family == 'tcp' and self.port.nil?
+          valid &= complain("Attribute 'port' must be specified for tcp sockets", self)
+        end
+        if self.family == 'unix' and self.path.nil?
+          valid &= complain("Attribute 'path' must be specified for unix sockets", self)
+        end
         valid
       end
       
       def test
-        false
+        socket = Socket.new(AF_INET, SOCK_STREAM, 0)
+        sockaddr = Socket.pack_sockaddr_in(self.port, self.addr)
+        retval = socket.connect(sockaddr)
+        close = socket.close()
+        retval == 0 ? true : false
       end
     end
     
--- test/test_conditions_socket_responding.rb
+++ test/test_conditions_socket_responding.rb
@@ -1,16 +1,65 @@
 require File.dirname(__FILE__) + '/helper'
 
 class TestConditionsSocketResponding < Test::Unit::TestCase
-#  def setup
-#  #@socket = God::Conditions::SocketResponding.new
-#    @socket = Conditions::ProcessRunning.new
-#  end
-
   # valid?
 
-  def test_valid_should_return_true_if_required_options_set
+  def test_valid_should_return_false_if_no_options_set
     c = Conditions::SocketResponding.new
-    c.valid?
+    c.watch = stub(:name => 'foo')
+    assert_equal false, c.valid?
+
   end
 
+  def test_valid_should_return_true_if_required_options_set_for_default
+    c = Conditions::SocketResponding.new
+    c.port = 443
+    assert_equal true, c.valid?
+  end
+
+  def test_valid_should_return_true_if_required_options_set_for_tcp
+    c = Conditions::SocketResponding.new
+    c.family = 'tcp'
+    c.port = 443
+    assert_equal true, c.valid?
+  end
+
+  def test_valid_should_return_true_if_required_options_set_for_unix
+    c = Conditions::SocketResponding.new
+    c.path = 'some-path'
+    c.family = 'unix'
+    assert_equal true, c.valid?
+  end
+
+  def test_valid_should_return_true_if_family_is_tcp
+    c = Conditions::SocketResponding.new
+    c.port = 443
+    c.family = 'tcp'
+    assert_equal true, c.valid?
+  end
+
+  def test_valid_should_return_true_if_family_is_unix
+    c = Conditions::SocketResponding.new
+    c.path = 'some-path'
+    c.family = 'unix'
+    c.watch = stub(:name => 'foo')
+    assert_equal true, c.valid?
+  end
+
+  # test
+
+  def test_test_should_return_true_if_socket_is_listening
+    c = Conditions::SocketResponding.new
+    c.port = 3000
+
+#    c.expects(:`).returns(0)
+    assert_equal true, c.test
+  end
+
+  def test_test_should_return_false_if_no_socket_is_listening
+    c = Conditions::SocketResponding.new
+    c.port = 80
+
+#    c.expects(:`).returns(-1)
+    assert_equal false, c.test
+  end
 end
