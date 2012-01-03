18ff6032e178f4f6615777a7afb1d0292238e956
Tom Preston-Werner <tom@mojombo.com>
2012-01-02 17:56:08 -0700

    Add decent Driver test.

--- test/test_driver.rb
+++ test/test_driver.rb
@@ -5,7 +5,21 @@ class TestDriver < Test::Unit::TestCase
 
   end
 
-  def test_
+  def test_push_pop_wait
+    eq = God::DriverEventQueue.new
 
+    MonitorMixin::ConditionVariable.any_instance.expects(:wait).times(1)
+
+    eq.push(God::TimedEvent.new(0))
+    eq.push(God::TimedEvent.new(0.1))
+    t = Thread.new do
+      # This pop will see an event immediately available, so no wait.
+      assert_equal TimedEvent, eq.pop.class
+
+      # This pop will happen before the next event is due, so wait.
+      assert_equal TimedEvent, eq.pop.class
+    end
+
+    t.join
   end
 end
