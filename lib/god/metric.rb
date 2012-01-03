e5ebda774a278d9886d30c29c37cb34309e036ff
Tom Preston-Werner <tom@mojombo.com>
2012-01-02 22:59:00 -0700

    TomDoc and clean up Metric class.

--- lib/god/metric.rb
+++ lib/god/metric.rb
@@ -1,59 +1,87 @@
 module God
-
+  # Metrics are responsible for holding watch conditions. An instance of
+  # Metric is yielded to blocks in the start_if, restart_if, stop_if, and
+  # transition methods.
   class Metric
-    attr_accessor :watch, :destination, :conditions
+    # The Watch.
+    attr_accessor :watch
 
+    # The destination Hash in canonical hash form. Example:
+    # { true => :up, false => :restart}
+    attr_accessor :destination
+
+    # The Array of Condition instances.
+    attr_accessor :conditions
+
+    # Initialize a new Metric.
+    #
+    # watch       - The Watch.
+    # destination - The optional destination Hash in canonical hash form.
     def initialize(watch, destination = nil)
       self.watch = watch
       self.destination = destination
       self.conditions = []
     end
 
-    # Instantiate a Condition of type +kind+ and pass it into the optional
-    # block. Attributes of the condition must be set in the config file
+    # Public: Instantiate the given Condition and pass it into the optional
+    # block. Attributes of the condition must be set in the config file.
+    #
+    # kind - The Symbol name of the condition.
+    #
+    # Returns nothing.
     def condition(kind)
-      # create the condition
+      # Create the condition.
       begin
         c = Condition.generate(kind, self.watch)
       rescue NoSuchConditionError => e
         abort e.message
       end
 
-      # send to block so config can set attributes
+      # Send to block so config can set attributes.
       yield(c) if block_given?
 
-      # call prepare on the condition
+      # Prepare the condition.
       c.prepare
 
-      # test generic and specific validity
+      # Test generic and specific validity.
       unless Condition.valid?(c) && c.valid?
         abort "Exiting on invalid condition"
       end
 
-      # inherit interval from watch if no poll condition specific interval was set
+      # Inherit interval from watch if no poll condition specific interval was
+      # set.
       if c.kind_of?(PollCondition) && !c.interval
         if self.watch.interval
           c.interval = self.watch.interval
         else
-          abort "No interval set for Condition '#{c.class.name}' in Watch '#{self.watch.name}', and no default Watch interval from which to inherit"
+          abort "No interval set for Condition '#{c.class.name}' in Watch " +
+                "'#{self.watch.name}', and no default Watch interval from " +
+                "which to inherit."
         end
       end
 
-      # remember
+      # Add the condition to the list.
       self.conditions << c
     end
 
+    # Enable all of this Metric's conditions. Poll conditions will be
+    # scheduled and event/trigger conditions will be registered.
+    #
+    # Returns nothing.
     def enable
       self.conditions.each do |c|
         self.watch.attach(c)
       end
     end
 
+    # Disable all of this Metric's conditions. Poll conditions will be
+    # halted and event/trigger conditions will be deregistered.
+    #
+    # Returns nothing.
     def disable
       self.conditions.each do |c|
         self.watch.detach(c)
       end
     end
   end
-
 end
