abba4705daa7b2c95414525b1c7788c23dda670b
Stevie Graham <sjtgraham@mac.com>
2012-01-18 19:40:54 +0000

    test coverage for twilio contact

--- test/test_twilio.rb
+++ test/test_twilio.rb
@@ -10,13 +10,17 @@ class TestTwilio < Test::Unit::TestCase
   end
 
   def test_notify
-  	@twilio.account_sid = 'ACXXXXX'
-  	@twilio.auth_token = 'YYYYYY'
-  	@twilio.from_number = '1234567890'
-    @twilio.to_number = '9876543210'
-    time = Time.now
-    Twilio.expects(:notify).returns(mock(:sid => 'ACXXXXX'))
-    @twilio.notify('msg', time, 'prio', 'cat', 'host')
-    assert_equal "sent txt message to 9876543210", @twilio.info
+    @twilio.account_sid = 'ACXXXXX'
+    @twilio.auth_token  = 'YYYYYY'
+    @twilio.from_number = '1234567890'
+    @twilio.to_number   = '9876543210'
+
+    # law of demeter much?
+    resource = mock.expects(:create).with :from => @twilio.from_number, :to => @twilio.to_number, :body => 'msg'
+    client   = mock(:account => mock(:sms => mock(:messages => resource)))
+
+    ::Twilio::REST::Client.expects(:new).with(@twilio.account_sid, @twilio.auth_token).returns client
+
+    @twilio.notify('msg', Time.now, 'prio', 'cat', 'host')
   end
 end
