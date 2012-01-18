8a8ed591fb048e3c368195b69ed0f6bdf3c3588e
Jonathan Gottfried <jonmarkgo@gmail.com>
2012-01-18 10:31:40 -0800

    Changed test to mock credentials

--- test/test_twilio.rb
+++ test/test_twilio.rb
@@ -10,12 +10,13 @@ class TestTwilio < Test::Unit::TestCase
   end
 
   def test_notify
-  	@twilio.account_sid = 'AC6d969d749c7e4e0bb5eb8968b85fa759'
-  	@twilio.auth_token = '1c43e9e653a181a3180083297ca6cf81'
-  	@twilio.from_number = '4069604079'
-    @twilio.to_number = '9406004450'
+  	@twilio.account_sid = 'ACXXXXX'
+  	@twilio.auth_token = 'YYYYYY'
+  	@twilio.from_number = '1234567890'
+    @twilio.to_number = '9876543210'
     time = Time.now
+    Twilio.expects(:notify).returns(mock(:sid => 'ACXXXXX'))
     @twilio.notify('msg', time, 'prio', 'cat', 'host')
-    assert_equal "sent txt message to 9406004450", @twilio.info
+    assert_equal "sent txt message to 9876543210", @twilio.info
   end
 end
