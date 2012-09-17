8733f75a9d4e737e7286dd617f4a67272cb483d8
Tom Preston-Werner <tom@mojombo.com>
2012-09-17 11:29:23 -0700

    Release 0.13.0

--- lib/god.rb
+++ lib/god.rb
@@ -151,7 +151,7 @@ end
 
 module God
   # The String version number for this package.
-  VERSION = '0.12.1'
+  VERSION = '0.13.0'
 
   # The Integer number of lines of backlog to keep for the logger.
   LOG_BUFFER_SIZE_DEFAULT = 100
