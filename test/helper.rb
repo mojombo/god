845d8ab62857713c04460e7bbdb31ce9956a8328
Eric Lindvall <eric@5stops.com>
2013-07-03 10:53:07 -0700

    Try to make 1.9 tests exit properly

--- test/helper.rb
+++ test/helper.rb
@@ -143,3 +143,17 @@ class Object
     Bypass.new(self)
   end
 end
+
+# Make sure we return valid exit codes
+if defined?(RUBY_ENGINE) && RUBY_ENGINE == "ruby" && RUBY_VERSION >= "1.9"
+  module Kernel
+    alias :__at_exit :at_exit
+    def at_exit(&block)
+      __at_exit do
+        exit_status = $!.status if $!.is_a?(SystemExit)
+        block.call
+        exit exit_status if exit_status
+      end
+    end
+  end
+end
