f48d0292f92146b0647035e13d4fdbe7f5f26c87
Scott Dubinsky <sdubinsky@gmail.com>
2014-09-04 17:17:44 -0400

    Updates timing.

--- lib/god/cli/run.rb
+++ lib/god/cli/run.rb
@@ -123,10 +123,10 @@ module God
           File.open(@options[:pid], 'w') { |f| f.write pid }
         end
 				#sleep a few second to make sure that the godfile has a chance to be read
-				sleep(3)
+				sleep 3
 				#returns nil if the process hasn't ended yet
 				if Process.waitpid(pid, Process::WNOHANG)
-					puts "Likely an error with the .god file.  In any event, the god process has already exited."
+					puts "There is likely an error with the .god file.  In any event, the god process has already exited."
 				end
         ::Process.detach pid
 
