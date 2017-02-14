c473791ad4dc4b1c6f37dc8eefe002343be46660
Ryan Buckley <arebuckley@gmail.com>
2017-02-14 10:13:12 -0800

    Downcase contact constant when requiring

--- lib/god/contacts.rb
+++ lib/god/contacts.rb
@@ -1,6 +1,6 @@
 module God::Contacts
   def self.const_missing(name)
-    require "god/contacts/#{name}"
+    require "god/contacts/#{name.to_s.downcase}"
     ::CONTACT_LOAD_SUCCESS[name] = true
     const_get(name) if const_defined?(name, false)
   rescue LoadError
